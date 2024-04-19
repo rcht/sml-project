4e163f4cd59fe3217be96736c0e28c0d25107535
Sammy459 <gupta.samridh.484@gmail.com>
2024-04-19 20:32:50 +0530

    lda done

--- lda.py
+++ lda.py
@@ -7,4 +7,3 @@ lda.fit(process_data.train_dataset.T, process_data.y_train)
 lda_predictions = lda.predict(process_data.test_dataset.T)
 accuracy = accuracy_score(process_data.y_test, lda_predictions)
 print("LDA Accuracy:", accuracy)
-
