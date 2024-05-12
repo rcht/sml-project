0bb1ff88192454019115d5ba2a3792429dd482f8
Sammy459 <gupta.samridh.484@gmail.com>
2024-05-12 22:54:31 +0530

    plots

--- AdaBoost.py
+++ AdaBoost.py
@@ -1,25 +1,20 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
 import process_data
+i = 99
+ada = AdaBoostClassifier(n_estimators=i+1, random_state=i+1, algorithm='SAMME')
+ada.fit(process_data.train_dataset.T, process_data.y_train)
+ada_predictions = ada.predict(process_data.test_dataset.T)
+ada_accuracy = accuracy_score(process_data.y_test, ada_predictions)
 
-adaboost_accuracies = []
-for i in range(0, 10):
-    ada = AdaBoostClassifier(n_estimators=i+1, random_state=i+1, algorithm='SAMME', learning_rate=0.1*(i+1))
-    ada.fit(process_data.train_dataset.T, process_data.y_train)
-    ada_predictions = ada.predict(process_data.test_dataset.T)
-    ada_accuracy = accuracy_score(process_data.y_test, ada_predictions)
-    adaboost_accuracies.append(ada_accuracy)
+ada1 = AdaBoostClassifier(n_estimators=i+1, random_state=i+1, algorithm='SAMME')
+ada1.fit(process_data.X_train1.T, process_data.y_train1)
+ada_predictions1 = ada1.predict(process_data.X_test1.T)
+ada_accuracy1 = accuracy_score(process_data.y_test1, ada_predictions1)
 
-adaboost_accuracies1 = []
-for i in range(0, 10):
-    ada1 = AdaBoostClassifier(n_estimators=i+1, random_state=i+1, algorithm='SAMME', learning_rate=0.1*(i+1))
-    ada1.fit(process_data.X_train1.T, process_data.y_train1)
-    ada_predictions1 = ada1.predict(process_data.X_test1.T)
-    ada_accuracy1 = accuracy_score(process_data.y_test1, ada_predictions1)
-    adaboost_accuracies1.append(ada_accuracy1)
 
-ada = max(adaboost_accuracies)
-ada1 = max(adaboost_accuracies1)
+ada = ada_accuracy
+ada1 = ada_accuracy1
 if __name__ == '__main__':
-    print(adaboost_accuracies)
-    print(adaboost_accuracies1)
+    print(ada_accuracy)
+    print(ada_accuracy1)
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import LogisticRegression
 import AdaBoost
 import Bagging
 
-
+dictionary = {0: "Random Forest", 1: "Decision Tree", 2: "QDA", 3: "Logistic Regression", 4: "LDA", 5: "AdaBoost",
+              6: "Bagging"}
 print("Test Accuracy Results for Dataset 1:")
-ada = AdaBoost.adaboost_accuracies
 print("Random Forest Accuracy:", RandomForest.accuracy)
 print("Decision Tree Accuracy:", DecisionTree.dt)
 print("LDA Accuracy:", lda.accuracy)
@@ -17,8 +17,11 @@ print("Logistic Regression Accuracy:", LogisticRegression.accuracy)
 print("AdaBoost Accuracy:", AdaBoost.ada)
 print("Bagging Accuracy:", Bagging.bagging_accuracy)
 print()
-print("Best Accuracy:", max(RandomForest.accuracy, DecisionTree.dt, lda.accuracy, qda.qda_accuracy,
-                            LogisticRegression.accuracy, AdaBoost.ada, Bagging.bagging_accuracy))
+Accuracy = [RandomForest.accuracy, DecisionTree.dt, qda.qda_accuracy, LogisticRegression.accuracy,lda.accuracy,
+            AdaBoost.ada, Bagging.bagging_accuracy]
+Model = dictionary[Accuracy.index(max(Accuracy))]
+print("Best Model:", Model)
+print("Best Accuracy:", max(Accuracy))
 
 print()
 print("Test Accuracy Results for Dataset 2:")
@@ -30,6 +33,21 @@ print("Logistic Regression Accuracy:", LogisticRegression.accuracy1)
 print("LDA Accuracy:", lda.accuracy1)
 print("QDA Accuracy:", qda.qda_accuracy1)
 print()
-print("Best Accuracy:", max(RandomForest.accuracy1, DecisionTree.dt1, lda.accuracy1, qda.qda_accuracy1,
-                            LogisticRegression.accuracy1, AdaBoost.ada1, Bagging.bagging_accuracy1))
-
+Accuracy1 = [RandomForest.accuracy1, DecisionTree.dt1, qda.qda_accuracy1, LogisticRegression.accuracy1, lda.accuracy1,
+             AdaBoost.ada1, Bagging.bagging_accuracy1]
+Model1 = dictionary[Accuracy1.index(max(Accuracy1))]
+print("Best Model:", Model1)
+print("Best Accuracy:", max(Accuracy1))
+BP = input("Enter 1 if High Blood Pressure 0 if not: ")
+Chol = input("Enter 0 if low cholestrol and 1 if high: ")
+BMI1 = input("Enter BMI: ")
+Smoking = input("Enter 1 if smoker 0 if not: ")
+Diabetes = input("Enter 1 if diabetic 0 if not: ")
+Alcohol = input("Enter 1 if heavy alcohol consumer 0 if not: ")
+Sex1 = input("Enter 0 if female 1 if male: ")
+Walk = input("Enter 1 if difficulty walking 0 if not: ")
+Age1 = input("Enter Age: ")
+Input = {"HighBp": BP, "HighChol": Chol, "CholCheck": 1, "BMI": BMI1, "Smoking": Smoking, "Stroke": 0,
+         "Diabetes": Diabetes, "Physical Activity": 1, "Fruits": 1, "Veggies": 1, "HvyAlcoholConsump": Alcohol,
+         "AnyHealthCare": 0, "NoDocbcCost": 0, "GenHlth": 3, "MentHlth": 18, "PhysHlth": 20, "DiffWalk": Walk,
+         "Sex": Sex1, "Age": Age1, "Education": 4, "Income": 4}
