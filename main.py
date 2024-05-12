7092cde455997e37ca5f09a0603b59101b8cf1a5
Sammy459 <gupta.samridh.484@gmail.com>
2024-05-13 00:52:17 +0530

    plots

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
-# import RandomForest
+import RandomForest
 import DecisionTree
-# import lda
-# import qda
-# import LogisticRegression
-# import AdaBoost
-# import Bagging
-
+import lda
+import qda
+import LogisticRegression
+import AdaBoost
+import Bagging
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
 import numpy as np
 
 dictionary = {0: "Random Forest", 1: "Decision Tree", 2: "QDA", 3: "Logistic Regression", 4: "LDA", 5: "AdaBoost",
               6: "Bagging"}
 print("Test Accuracy Results for Dataset 1:")
-# print("Random Forest Accuracy:", RandomForest.accuracy)
-print("Decision Tree Accuracy:", DecisionTree.dt)
-# print("LDA Accuracy:", lda.accuracy)
-# print("QDA Accuracy:", qda.qda_accuracy)
-# print("Logistic Regression Accuracy:", LogisticRegression.accuracy)
-# print("AdaBoost Accuracy:", AdaBoost.ada)
-# print("Bagging Accuracy:", Bagging.bagging_accuracy)
+print("Random Forest Accuracy:", RandomForest.accuracy)
+print("Decision Tree Accuracy:", DecisionTree.dt_acc)
+print("LDA Accuracy:", lda.accuracy)
+print("QDA Accuracy:", qda.qda_accuracy)
+print("Logistic Regression Accuracy:", LogisticRegression.accuracy)
+print("AdaBoost Accuracy:", AdaBoost.ada)
+print("Bagging Accuracy:", Bagging.bagging_accuracy)
 print()
-# Accuracy = [RandomForest.accuracy, DecisionTree.dt, qda.qda_accuracy, LogisticRegression.accuracy,lda.accuracy,
-            # AdaBoost.ada, Bagging.bagging_accuracy]
-Accuracy = [DecisionTree.dt]
+Accuracy = [RandomForest.accuracy, DecisionTree.dt_acc, qda.qda_accuracy, LogisticRegression.accuracy,lda.accuracy,
+            AdaBoost.ada, Bagging.bagging_accuracy]
 Model = dictionary[Accuracy.index(max(Accuracy))]
 print("Best Model:", Model)
 print("Best Accuracy:", max(Accuracy))
 
 print()
 print("Test Accuracy Results for Dataset 2:")
-# print("Random Forest Accuracy:", RandomForest.accuracy1)
+print("Random Forest Accuracy:", RandomForest.accuracy1)
 print("Decision Tree Accuracy:", DecisionTree.dt1_acc)
-# print("AdaBoost Accuracy:", AdaBoost.ada1)
-# print("Bagging Accuracy:", Bagging.bagging_accuracy1)
-# print("Logistic Regression Accuracy:", LogisticRegression.accuracy1)
-# print("LDA Accuracy:", lda.accuracy1)
-# print("QDA Accuracy:", qda.qda_accuracy1)
+print("AdaBoost Accuracy:", AdaBoost.ada1)
+print("Bagging Accuracy:", Bagging.bagging_accuracy1)
+print("Logistic Regression Accuracy:", LogisticRegression.accuracy1)
+print("LDA Accuracy:", lda.accuracy1)
+print("QDA Accuracy:", qda.qda_accuracy1)
 print()
-# Accuracy1 = [RandomForest.accuracy1, DecisionTree.dt1_acc, qda.qda_accuracy1, LogisticRegression.accuracy1, lda.accuracy1,
-             # AdaBoost.ada1, Bagging.bagging_accuracy1]
-Accuracy1 = [DecisionTree.dt1_acc]
+Accuracy1 = [RandomForest.accuracy1, DecisionTree.dt1_acc, qda.qda_accuracy1, LogisticRegression.accuracy1, lda.accuracy1,
+             AdaBoost.ada1, Bagging.bagging_accuracy1]
 Model1 = dictionary[Accuracy1.index(max(Accuracy1))]
 print("Best Model:", Model1)
 print("Best Accuracy:", max(Accuracy1))
@@ -63,7 +62,8 @@ while True:
     # Walk = input("Enter 1 if difficulty walking 0 if not: ")
     Sex1 = input("Enter 0 if female 1 if male: ").strip()
     Sex1 = int(Sex1)
-    Age1 = float(input("Enter Age: "))
+    print("Age Ranges: 1: 10-15 2: 16-20 3: 21-30 4: 31-40 5: 41-50 6: 51-60 7: 61-70 8: 71-80 9: 81-90 10: 91-100")
+    Age1 = float(input("Enter Age Range: "))
     Input = {"HighBp": BP, "HighChol": Chol, "CholCheck": 1, "BMI": BMI1, "Smoking": Smoking, "Stroke": 0,
              "Diabetes": Diabetes, "Physical Activity": (1 if BMI1 < 25 else 0), "Fruits": 1, "Veggies": (1 if BMI1 < 25 else 0), "HvyAlcoholConsump": Alcohol,
              "AnyHealthCare": 0, "NoDocbcCost": 0, "GenHlth": 3, "MentHlth": (0 if Alcohol else 18) , "PhysHlth": (20 if BMI1 < 25 and Walk == 0 else 0), "DiffWalk": Walk,
@@ -71,4 +71,10 @@ while True:
     InputArr = np.array([i for i in Input.values()])
     prediction = DecisionTree.dt1.predict(InputArr.reshape(1, -1))
     print(prediction)
-    
+plt.figure(figsize=(10, 6))
+plot_tree(DecisionTree.dt1, filled=True)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plot_tree(DecisionTree.dt, filled=True)
+plt.show()
